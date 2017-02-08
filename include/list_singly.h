e12f8da9caff4912546ceee2d06f5ac37be7b240
Walt Elam <wrelam@gmail.com>
2017-02-07 22:04:34 -0500

    Added for_each iterator

--- include/list_singly.h
+++ include/list_singly.h
@@ -2,6 +2,8 @@
     @file   list_singly.h
 
     @brief  Interface to a singly linked list implementation
+
+    When used, an sll_node/list should be the first member in a struct.
 *******************************************************************************/
 #ifndef LIST_SINGLY_H
 #define LIST_SINGLY_H
@@ -15,6 +17,20 @@ typedef struct sll_node {
 typedef struct sll_list {
     struct sll_node *head;
 } sll_list;
+
+/*******************************************************************************
+    sll_for_each
+*//**
+    @brief  Iterates over an sll_list
+    @param  _listPtr   Pointer to an sll_list struct to iterate over
+    @param  _curPtr     Pointer to a variable holding each member of the list
+    @param  _type       Type of _cur
+    @param  _member     sll_node member name within _type
+*******************************************************************************/
+#define sll_for_each(_listPtr, _curPtr, _type, _member) \
+    for ((_curPtr) = (_type) (_listPtr)->head;          \
+         NULL != (_curPtr);                             \
+         (_curPtr) = (_type) (_curPtr)->_member.next)
 
 sll_node *
 sll_insert_after(sll_node *elem, sll_node *new);
