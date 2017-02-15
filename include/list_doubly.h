d3041218763e680ab60a5ad1e015e44974a0c7c9
Walt Elam <wrelam@gmail.com>
2017-02-15 01:05:23 -0500

    Converted doubly linked list to use pointers and finished the implementation for now

--- include/list_doubly.h
+++ include/list_doubly.h
@@ -8,33 +8,82 @@
 
 #include <string.h>
 
-#ifndef NULL_NODE
-#define NULL_NODE   NULL
-#endif /* NULL_NODE */
-
 typedef struct dll_node {
     struct dll_node *next;
     struct dll_node *prev;
     void *data;
-} * dll_node;
+} dll_node;
 
-dll_node
-dll_create_node(void);
+typedef struct dll_list {
+    struct dll_node *head;
+} dll_list;
+
+/*******************************************************************************
+    dll_for_each
+*//**
+    @brief  Iterates over a dll_list
+    @param  _listPtr    Pointer to an dll_list struct to iterate over
+    @param  _curPtr     Pointer to a variable holding each member of the list
+    @param  _type       Type of _cur
+    @param  _member     dll_node member name within _type
+    @param  _tmpPtr     Temporary pointer to prevent issues when deleting nodes
+
+    The ", 1" ensures that the only condition that applies in the NULL check on
+    curPtr and that a NULL next entry does not prematurely stop the loop.
+*******************************************************************************/
+#define dll_for_each(_listPtr, _curPtr, _type, _member, _tmpPtr)               \
+    for ((_curPtr) = (_type) (_listPtr)->head;                                 \
+         NULL != (_curPtr) && ((_tmpPtr) = (_type) (_curPtr)->_member.next, 1);\
+
+/*******************************************************************************
+    dll_for_each_rev
+*//**
+    @brief  Iterates over a dll_list in reverse
+    @param  _listPtr    Pointer to an dll_list struct to iterate over
+    @param  _curPtr     Pointer to a variable holding each member of the list
+    @param  _type       Type of _cur
+    @param  _member     dll_node member name within _type
+    @param  _tmpPtr     Temporary pointer to prevent issues when deleting nodes
+
+    The ", 1" ensures that the only condition that applies in the NULL check on
+    curPtr and that a NULL next entry does not prematurely stop the loop.
+*******************************************************************************/
+#define dll_for_eachrev(_listPtr, _curPtr, _type, _member, _tmpPtr)            \
+    dll_node *_backPtr = (_listPtr)->head;                                     \
+    while (NULL != _backPtr->next)                                             \
+    {                                                                          \
+        _backPtr = backPtr->next;                                              \
+    }                                                                          \
+    for ((_curPtr) = (_type) _backPtr;                                         \
+         NULL != (_curPtr) && ((_tmpPtr) = (_type) (_curPtr)->_member.prev, 1);\
+         (_curPtr) = (_tmpPtr))
+
+dll_node *
+dll_insert_after(dll_node *elem, dll_node *new);
+
+dll_list *
+dll_insert_back(dll_list *list, dll_node * new);
+
+dll_node *
+dll_insert_before(dll_node *elem, dll_node *new);
+
+dll_list *
+dll_insert_front(dll_list *list, dll_node *new);
+
+int
+dll_is_empty(dll_list *list);
 
 void
-dll_destroy_node(dll_node elem);
+dll_remove(dll_node *elem);
 
-dll_node
-dll_insert_after(dll_node elem, dll_node new);
+dll_list *
+dll_remove_back(dll_list *list);
 
-dll_node
-dll_insert_at_end(dll_node elem, dll_node new);
+dll_list *
+dll_remove_front(dll_list *list);
 
-dll_node
-dll_insert_before(dll_node elem, dll_node new);
-
-void
-dll_remove(dll_node elem);
+dll_list *
+dll_reverse(dll_list *list);
 
 #endif /* LIST_DOUBLY_H */
 
--- src/list_doubly.c
+++ src/list_doubly.c
@@ -8,105 +8,146 @@
 #include "list_doubly.h"
 
 /*******************************************************************************
-    dll_create_node
-*//**
-    @brief  Allocates and returns an initialized node
-
-    @return A new doubly linked list node
-    @retval NULL_NODE   Failed to allocate new node
-*******************************************************************************/
-dll_node
-dll_create_node(void)
-{
-    dll_node new = calloc(1, sizeof(struct dll_node));
-
-    if (NULL != new)
-    {
-        new->next = NULL_NODE;
-        new->prev = NULL_NODE;
-        new->data = NULL;
-    }
-
-    return new;
-}
-
-
-/*******************************************************************************
-    dll_destroy_node
-*//**
-    @brief  Destroys a given node
-    @param  elem    List node being destroyed
-*******************************************************************************/
-void
-dll_destroy_node(dll_node elem)
-{
-    free(elem);
-}
-
-
-/*******************************************************************************
     dll_insert_after
 *//**
     @brief  Inserts one node after another
-    @param  elem    List node having a node inserted after it
-    @param  new     List node being inserted
+    @param  *elem   List node after which the new node is inserted
+    @param  *new    List node being inserted
 
-    @return The frontmost node of the pair
+    @return Frontmost node of the pair
+    @retval NULL    Given node was NULL
 *******************************************************************************/
-dll_node
-dll_insert_after(dll_node elem, dll_node new)
+dll_node *
+dll_insert_after(dll_node *elem, dll_node *new)
 {
-    new->prev = elem;
-    new->next = elem->next;
-    elem->next->prev = new;
-    elem->next = new;
+    if ((NULL != elem) &&
+        (NULL != new))
+    {
+        new->prev = elem;
+        new->next = elem->next;
+        if (NULL != elem->next)
+        {
+            elem->next->prev = new;
+        }
+        elem->next = new;
+    }
+
     return elem;
 }
 
 
 /*******************************************************************************
-    dll_insert_at_end
+    dll_insert_back
 *//**
     @brief  Inserts a node at the end of a list
-    @param  elem    Existing node in the list to be appended to
-    @param  new     List node being inserted
+    @param  *list   List to modify
+    @param  *new    List node being inserted
 
     @see    dll_insert_after()
 
-    @return The node defined by elem
+    @return Modified list
+    @retval NULL    List does not exist
 *******************************************************************************/
-dll_node
-dll_insert_at_end(dll_node elem, dll_node new)
+dll_list *
+dll_insert_back(dll_list *list, dll_node * new)
 {
-    dll_node iter = elem;
+    dll_node *iter = NULL;
 
-    while (NULL_NODE != iter->next)
+    if (NULL != list)
     {
-        iter = iter->next;
+        /* Adding the initial node to the list */
+        if (NULL == list->head)
+        {
+            list->head = new;
+        }
+        /* Find the last node and append */
+        else
+        {
+            iter = list->head;
+
+            while (NULL != iter->next)
+            {
+                iter = iter->next;
+            }
+
+            iter = dll_insert_after(iter, new);
+        }
     }
 
-    dll_insert_after(iter, new);
-
-    return elem;
+    return list;
 }
 
 
 /*******************************************************************************
     dll_insert_before
 *//**
-    @brief  Inserts one node after another
-    @param  elem    List node having a node inserted before it
-    @param  new     List node being inserted
+    @brief  Inserts one node before another
+    @param  *elem   List node before which a node will be added
+    @param  *new    List node being inserted
 
-    @return The frontmost node of the pair
+    @return Frontmost node of the pair
+    @retval NULL    Given node was NULL
 *******************************************************************************/
-dll_node
-dll_insert_before(dll_node elem, dll_node new)
+dll_node *
+dll_insert_before(dll_node *elem, dll_node *new)
 {
-    new->prev = elem->prev;
-    new->next = elem;
-    elem->prev = new;
+    if ((NULL != elem) &&
+        (NULL != new))
+    {
+        new->next = elem;
+        new->prev = elem->prev;
+        if (NULL != elem->prev)
+        {
+            elem->prev->next = new;
+        }
+        elem->prev = new;
+    }
+
     return new;
+}
+
+
+/*******************************************************************************
+    dll_insert_front
+*//**
+    @brief  Inserts a node at the beginning of a list
+    @param  *list   List to modify
+    @param  *new    List node being inserted
+
+    @return Modified list
+    @retval NULL    List does not exist
+*******************************************************************************/
+dll_list *
+dll_insert_front(dll_list *list, dll_node *new)
+{
+    if (NULL != list)
+    {
+        if (NULL != list->head)
+        {
+            new = dll_insert_after(new, list->head);
+        }
+
+        list->head = new;
+    }
+
+    return list;
+}
+
+
+/*******************************************************************************
+    dll_is_empty
+*//**
+    @brief  Determines if a list is empty or not
+    @param  *list   List to check
+
+    @return Boolean indicating emptiness
+    @retval 0   List is not empty
+    @retval !0  List is empty
+*******************************************************************************/
+int
+dll_is_empty(dll_list *list)
+{
+    return !((NULL != list) && (NULL != list->head));
 }
 
 
@@ -114,22 +155,119 @@ dll_insert_before(dll_node elem, dll_node new)
     dll_remove
 *//**
     @brief  Removes a node from the list to which it belongs
-    @param  elem    The list node to remove
+    @param  *elem   The list node to remove
 *******************************************************************************/
 void
-dll_remove(dll_node elem)
+dll_remove(dll_node *elem)
 {
-    if (NULL_NODE != elem)
+    if (NULL != elem)
     {
-        if (NULL_NODE != elem->next)
+        if (NULL != elem->next)
         {
             elem->next->prev = elem->prev;
+            elem->next = NULL;
         }
 
-        if (NULL_NODE != elem->prev)
+        if (NULL != elem->prev)
         {
             elem->prev->next = elem->next;
+            elem->prev = NULL;
+        }
+
+    }
+}
+
+
+/*******************************************************************************
+    dll_remove_back
+*//**
+    @brief  Removes the element at the back of the list
+    @param  *list   List from which the back element will be removed
+
+    @see    dll_remove()
+
+    @return Modified list
+    @retval NULL    List does not exist
+*******************************************************************************/
+dll_list *
+dll_remove_back(dll_list *list)
+{
+    dll_node *iter = NULL;
+
+    if (NULL != list)
+    {
+        iter = list->head;
+
+        while (NULL != iter->next)
+        {
+            iter = iter->next;
+        }
+
+        dll_remove(iter);
+    }
+
+    return list;
+}
+
+
+/*******************************************************************************
+    dll_remove_front
+*//**
+    @brief  Removes the element at the front of the list
+    @param  *list   List from which the front element will be removed
+
+    @see    dll_remove()
+
+    @return Modified list
+    @retval NULL    List does not exist
+*******************************************************************************/
+dll_list *
+dll_remove_front(dll_list *list)
+{
+    dll_node *newHead = NULL;
+
+    if (!dll_is_empty(list))
+    {
+        newHead = list->head->next;
+
+        if (NULL != list->head->next)
+        {
+            list->head->next->prev = NULL;
+        }
+
+        dll_remove(list->head);
+        list->head = newHead;
+    }
+
+    return list;
+}
+
+
+/*******************************************************************************
+    dll_reverse
+*//**
+    @brief  Reverses a list
+    @param  *list   List to reverse
+
+    @return Reversed list
+    @retval NULL    List does not exist
+*******************************************************************************/
+dll_list *
+dll_reverse(dll_list *list)
+{
+    dll_node *tmp = NULL;
+
+    if (!dll_is_empty(list))
+    {
+        while (NULL != list->head)
+        {
+            tmp = list->head->next;
+            list->head->next = list->head->prev;
+            list->head->prev = list->head->next;
+            list->head = tmp;
         }
     }
+
+    return list;
 }
 
